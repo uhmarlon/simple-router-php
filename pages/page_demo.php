e025110180f2fbfc64aac39442d06b24554624f0
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2020-11-18 18:32:11 +0100

    Add more under routers

--- pages/page_demo.php
+++ pages/page_demo.php
@@ -1,3 +1,12 @@
-<?php function Page() { ?>
+<?php
+//
+//if you want to add more under routers do so
+//
+// Routing on /demo/test/
+// if($_GET[1] == 'test') {
+// echo "Test";}
+//
+
+function Page() { ?>
      <h1> Demo </h1>
 <?php } ?>
